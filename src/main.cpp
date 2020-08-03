1ef5ab374e5d5edc684f03f787d89e6f96842727
Luciano Vittori <lucianovittori99@gmail.com>
2020-08-03 11:16:15 -0300

    temperatureScren

--- src/main.cpp
+++ src/main.cpp
@@ -82,52 +82,41 @@ U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, /* CS=*/STP7920_CS_PIN, /* reset= */ S
 u8g2_uint_t offset;            // current offset for the scrolling text
 u8g2_uint_t width;             // pixel width of the scrolling text (must be lesser than 128 unless U8G2_16BIT is defined
 const char *text = "Novotec "; // scroll this text from right to left
+void draw();
+const char DEGREE_SYMBOL[] = {0xB0, '\0'};
+char temperature[5] = "28";
+char humidity[5] = "80";
 
 void setup(void)
 {
-  pinMode(STP7920_CS_PIN, OUTPUT);
-  digitalWrite(STP7920_CS_PIN, 0);
-  pinMode(STP7920_CS_PIN, OUTPUT);
-  digitalWrite(STP7920_CS_PIN, 0);
-  pinMode(STP7920_RESET_PIN, OUTPUT);
-  digitalWrite(STP7920_RESET_PIN, 0);
-  pinMode(STP7920_RESET_PIN, OUTPUT);
-  digitalWrite(STP7920_RESET_PIN, 0);
-
   u8g2.begin();
-
-  u8g2.setFont(u8g2_font_inb30_mr); // set the target font to calculate the pixel width
-  width = u8g2.getUTF8Width(text);  // calculate the pixel width of the text
-
-  u8g2.setFontMode(false); // enable transparent mode, which is faster
+  u8g2.enableUTF8Print();
+  u8g2.setFont(u8g2_font_helvB10_tf);
+  u8g2.setColorIndex(1);
 }
 
-void loop(void)
+void loop()
 {
-  u8g2_uint_t x;
-
   u8g2.firstPage();
   do
   {
-
-    // draw the scrolling text at current offset
-    x = offset;
-    u8g2.setFont(u8g2_font_inb30_mr); // set the target font
-    do
-    {                                     // repeated drawing of the scrolling text...
-      u8g2.drawUTF8(x, 30, text);         // draw the scolling text
-      x += width;                         // add the pixel width of the scrolling text
-    } while (x < u8g2.getDisplayWidth()); // draw again until the complete display is filled
-
-    u8g2.setFont(u8g2_font_inb16_mr); // draw the current pixel width
-    u8g2.setCursor(0, 58);
-    u8g2.print(width); // this value must be lesser than 128 unless U8G2_16BIT is set
-
+    draw();
+    delay(30);
   } while (u8g2.nextPage());
+}
 
-  offset -= 1; // scroll by one pixel
-  if ((u8g2_uint_t)offset < (u8g2_uint_t)-width)
-    offset = 0; // start over again
+void draw()
+{
 
-  delay(100); // do some small delay
+  u8g2.drawFrame(0, 0, 128, 31);
+  u8g2.drawFrame(0, 33, 128, 31);
+
+  u8g2.drawStr(15, 13, "Temperature");
+  u8g2.drawStr(35, 28, temperature);
+  u8g2.drawUTF8(70, 28, DEGREE_SYMBOL);
+  u8g2.drawUTF8(76, 28, "C");
+
+  u8g2.drawStr(30, 46, "Humidity");
+  u8g2.drawStr(37, 61, humidity);
+  u8g2.drawStr(75, 61, "%");
 }
