ccf3b84d483fa3e962a09d23ab8087e67e9355c7
Luciano Vittori <lucianovittori99@gmail.com>
2020-08-03 11:38:39 -0300

    Novotec screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
   ScrollingText.ino
 
-  This will scroll text on the display. 
+  This will scroll text on the display.
   Enable U8g2 16 bit mode (see FAQ) for larger text!
 
   Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
@@ -10,29 +10,29 @@
   Copyright (c) 2016, olikraus@gmail.com
   All rights reserved.
 
-  Redistribution and use in source and binary forms, with or without modification, 
+  Redistribution and use in source and binary forms, with or without modification,
   are permitted provided that the following conditions are met:
 
-  * Redistributions of source code must retain the above copyright notice, this list 
+  * Redistributions of source code must retain the above copyright notice, this list
     of conditions and the following disclaimer.
-    
-  * Redistributions in binary form must reproduce the above copyright notice, this 
-    list of conditions and the following disclaimer in the documentation and/or other 
+
+  * Redistributions in binary form must reproduce the above copyright notice, this
+    list of conditions and the following disclaimer in the documentation and/or other
     materials provided with the distribution.
 
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
-  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
-  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
-  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
 
@@ -51,9 +51,9 @@
     Frame Buffer Examples: clearBuffer/sendBuffer. Fast, but may not work with all Arduino boards because of RAM consumption
     Page Buffer Examples: firstPage/nextPage. Less RAM usage, should work with all Arduino boards.
     U8x8 Text Only Example: No RAM usage, direct communication with display controller. No graphics, 8x8 Text only.
-    
+
   This is a page buffer example.
-  
+
 */
 
 // Please UNCOMMENT one of the contructor lines below
@@ -61,62 +61,25 @@
 // The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
 // Please update the pin numbers according to your setup. Use U8X8_PIN_NONE if the reset pin is not connected
 
-//U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 18 /* A4 */ , /* data=*/ 16 /* A2 */, /* CS=*/ 17 /* A3 */, /* reset=*/ U8X8_PIN_NONE);
-//U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
-//U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 14, /* data=*/ 13, /* CS=*/ 15, /* reset=*/ 16); // Feather HUZZAH ESP8266, E=clock=14, RW=data=13, RS=CS
-//U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, /* CS=*/ 10, /* reset=*/ 8);
-//U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, /* CS=*/ 15, /* reset=*/ 16); // Feather HUZZAH ESP8266, E=clock=14, RW=data=13, RS=CS
-
-// End of constructor list
-
-// This example shows a scrolling text.
-// If U8G2_16BIT is not set (default), then the pixel width of the text must be lesser than 128
-// If U8G2_16BIT is set, then the pixel width an be up to 32000
-
 #define STP7920_CS_PIN 5
 #define STP7920_RESET_PIN 19
 
-U8G2_ST7920_128X64_1_HW_SPI u8g2(U8G2_R0, /* CS=*/STP7920_CS_PIN, /* reset= */ STP7920_RESET_PIN);
-//U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 18, /* data=*/ 23, /* CS=*/ STP7920_CS_PIN, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 18, /* data=*/ 23, /* CS=*/ STP7920_CS_PIN, /* reset=*/ STP7920_RESET_PIN); // ESP32
+U8G2_ST7920_128X64_F_HW_SPI u8g2(U8G2_R0,/* CS=*/ STP7920_CS_PIN, /* reset=*/ STP7920_RESET_PIN); // ESP32
 
-u8g2_uint_t offset;            // current offset for the scrolling text
-u8g2_uint_t width;             // pixel width of the scrolling text (must be lesser than 128 unless U8G2_16BIT is defined
-const char *text = "Novotec "; // scroll this text from right to left
-void draw();
-const char DEGREE_SYMBOL[] = {0xB0, '\0'};
-char temperature[5] = "28";
-char humidity[5] = "80";
 
 void setup(void)
 {
-  u8g2.begin();
-  u8g2.enableUTF8Print();
-  u8g2.setFont(u8g2_font_helvB10_tf);
-  u8g2.setColorIndex(1);
+    u8g2.begin();
+    u8g2.setFont(u8g2_font_osb21_tf);
 }
 
 void loop()
 {
-  u8g2.firstPage();
-  do
-  {
-    draw();
-    delay(30);
-  } while (u8g2.nextPage());
-}
-
-void draw()
-{
-
-  u8g2.drawFrame(0, 0, 128, 31);
-  u8g2.drawFrame(0, 33, 128, 31);
-
-  u8g2.drawStr(15, 13, "Temperature");
-  u8g2.drawStr(35, 28, temperature);
-  u8g2.drawUTF8(70, 28, DEGREE_SYMBOL);
-  u8g2.drawUTF8(76, 28, "C");
-
-  u8g2.drawStr(30, 46, "Humidity");
-  u8g2.drawStr(37, 61, humidity);
-  u8g2.drawStr(75, 61, "%");
+    u8g2.firstPage();
+    do
+    {
+        u8g2.drawStr(14, 40, "Novotec");
+        delay(30);
+    } while (u8g2.nextPage());
 }
